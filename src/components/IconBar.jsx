33e626a1c6425a9575c8f9d4100756f7d7f2f1a0
DemoTheMann <joshua.bienvenu18@gmail.com>
2024-04-24 13:51:19 +0200

    updated icon bar

--- src/components/IconBar.jsx
+++ src/components/IconBar.jsx
@@ -1,21 +1,20 @@
-import React from 'react';
-import './IconeBar.css';
+import React from "react";
+import "./IconeBar.css";
 
 const IconBar = ({ onCategoryChange }) => {
-  
   const handleIconClick = (category) => {
     onCategoryChange(category);
   };
 
   return (
-    <div className="icon-bar">
-      <div onClick={() => handleIconClick('Litteraire')}>📚</div>
-      <div onClick={() => handleIconClick('Sport')}>🏀</div>
-      <div onClick={() => handleIconClick('Lesplusconnus')}>🌟</div>
-      <div onClick={() => handleIconClick('HommesHistoire')}>🎩</div>
-      <div onClick={() => handleIconClick('Acteurs')}>🎭</div>
-      <div onClick={() => handleIconClick('Chanteur')}>🎤</div>
-      <div onClick={() => handleIconClick('Hommes politique')}>🏛️</div>
+    <div className="w-full shadow-xl text-2xl p-2 flex justify-between items-center rounded-lg">
+      <div onClick={() => handleIconClick("Litteraire")}>📚</div>
+      <div onClick={() => handleIconClick("Sport")}>🏀</div>
+      <div onClick={() => handleIconClick("Lesplusconnus")}>🌟</div>
+      <div onClick={() => handleIconClick("HommesHistoire")}>🎩</div>
+      <div onClick={() => handleIconClick("Acteurs")}>🎭</div>
+      <div onClick={() => handleIconClick("Chanteur")}>🎤</div>
+      <div onClick={() => handleIconClick("Hommes politique")}>🏛️</div>
     </div>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { FaUserCircle } from "react-icons/fa";
 // import "./Navbar.css";
 import LangManager from "./LangManager";
 import { useTranslation } from "react-i18next";
+import CMT from "../assets/img/svg/CMT-logo-text.svg";
 
 const Navbar = ({ onUserIconClick }) => {
   const { t } = useTranslation();
@@ -12,7 +13,10 @@ const Navbar = ({ onUserIconClick }) => {
       <div className="w-1/5 flex justify-center items-center">
         <LangManager />
       </div>
-      <h1 className="text-2xl font-bold">Catch My Tomb</h1>
+      {/* <h1 className="text-2xl font-bold">Catch My Tomb</h1> */}
+      <div className="w-2/4 p-2">
+        <img src={CMT} alt="Catch my Tomb" className="w-full" />
+      </div>
       <FaUserCircle
         className="w-1/5 text-5xl text-white"
         onClick={onUserIconClick}
