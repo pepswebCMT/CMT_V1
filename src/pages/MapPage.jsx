34abe6dc0030a60e2d0a5bf70487eed41f7a0ed9
DemoTheMann <joshua.bienvenu18@gmail.com>
2024-05-02 16:17:38 +0200

    added map clusters

--- src/pages/MapPage.jsx
+++ src/pages/MapPage.jsx
@@ -14,6 +14,7 @@ import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import { FaMapMarker } from "react-icons/fa";
 import tombstoneImage from "./styles/tombstone_1.png";
+import MarkerClusterGroup from "react-leaflet-cluster";
 
 const customMarkerHtml = renderToStaticMarkup(
   <div
@@ -58,27 +59,35 @@ const MyMap = () => {
   const [userLocation, setUserLocation] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-  const categories = ['HommesHistoire', 'Les plus connus', 'sport', 'Acteurs', 'Chanteur', 'Hommes politique', 'Litteraire'];
+  const categories = [
+    "HommesHistoire",
+    "Les plus connus",
+    "sport",
+    "Acteurs",
+    "Chanteur",
+    "Hommes politique",
+    "Litteraire",
+  ];
 
   useEffect(() => {
     setLoading(true);
     const fetchAllItems = async () => {
       try {
         const docRef = doc(db, "Tombs", "OccpEQD19eoOmrLfPaP0");
-        const promises = categories.map(category => {
+        const promises = categories.map((category) => {
           const colRef = collection(docRef, category);
           const q = query(colRef);
           return getDocs(q);
         });
 
         const snapshots = await Promise.all(promises);
-        const allItems = snapshots.flatMap(snapshot =>
-          snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }))
+        const allItems = snapshots.flatMap((snapshot) =>
+          snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
         );
 
         setItems(allItems);
       } catch (e) {
-        setError('Failed to fetch data');
+        setError("Failed to fetch data");
         console.error(e);
       } finally {
         setLoading(false);
@@ -99,85 +108,83 @@ const MyMap = () => {
     );
   }, []);
 
-if (loading) {
-  return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="text-xl font-semibold">REST IN PEACE...</div>
-    </div>
-  );
-}
-
-if (error) {
-  return (
-    <div className="flex items-center justify-center h-screen">
-      <div className="text-red-500 text-xl font-semibold">
-        Error: {error}
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <div className="text-xl font-semibold">REST IN PEACE...</div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 
+  if (error) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <div className="text-red-500 text-xl font-semibold">Error: {error}</div>
+      </div>
+    );
+  }
 
-return (
-  <MapContainer
-    center={[51.505, -0.09]}
-    zoom={13}
-    style={{ height: "100vh", width: "100%" }}
-  >
-    <TileLayer
-      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
-    />
-    {items.map((item) => (
-      <Marker
-        key={item.id}
-        position={[item.location._lat, item.location._long]}
-        icon={customMarkerIcon}
-      >
-<Popup>
-  <div className="text-center">
-    <strong>{item.title}</strong>
-    <div className="flex justify-center mt-2">
-      <img
-        src={item.imageUrl}
-        alt={item.title}
-        className="w-16 h-16 rounded-full object-cover" 
-      />
-    </div>
-    <br />
-    <button
-      onClick={() => {
-        window.open(
-          `https://www.google.com/maps/search/?api=1&query=${item.location._lat},${item.location._long}`,
-          "_blank"
-        );
-      }}
-      className="mt-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded inline-block"
+  return (
+    <MapContainer
+      center={[51.505, -0.09]}
+      zoom={13}
+      style={{ height: "100vh", width: "100%" }}
     >
-      Y aller
-    </button>
-  </div>
-</Popup>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+      />
+      <MarkerClusterGroup chunkedLoading>
+        {items.map((item) => (
+          <Marker
+            key={item.id}
+            position={[item.location._lat, item.location._long]}
+            icon={customMarkerIcon}
+          >
+            <Popup>
+              <div className="text-center">
+                <strong>{item.title}</strong>
+                <div className="flex justify-center mt-2">
+                  <img
+                    src={item.imageUrl}
+                    alt={item.title}
+                    className="w-16 h-16 rounded-full object-cover"
+                  />
+                </div>
+                <br />
+                <button
+                  onClick={() => {
+                    window.open(
+                      `https://www.google.com/maps/search/?api=1&query=${item.location._lat},${item.location._long}`,
+                      "_blank"
+                    );
+                  }}
+                  className="mt-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded inline-block"
+                >
+                  Y aller
+                </button>
+              </div>
+            </Popup>
+          </Marker>
+        ))}
+      </MarkerClusterGroup>
 
-      </Marker>
-    ))}
-    {userLocation && (
-      <>
-        <CircleMarker
-          center={userLocation}
-          color="blue"
-          radius={2}
-          fillColor="#0000FF"
-          fillOpacity={1}
-        >
-          <Popup>Vous êtes ici</Popup>
-        </CircleMarker>
-        <SetViewOnClick coords={userLocation} />
-      </>
-    )}
-  </MapContainer>
-);
-
+      {userLocation && (
+        <>
+          <CircleMarker
+            center={userLocation}
+            color="blue"
+            radius={2}
+            fillColor="#0000FF"
+            fillOpacity={1}
+          >
+            <Popup>Vous êtes ici</Popup>
+          </CircleMarker>
+          <SetViewOnClick coords={userLocation} />
+        </>
+      )}
+    </MapContainer>
+  );
 };
 
 export default MyMap;
