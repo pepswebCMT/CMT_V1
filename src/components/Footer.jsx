e709458f6a8d5757ddaa8d803908dd4ad34a82b6
akimi987 <75440087+akimi987@users.noreply.github.com>
2024-06-04 08:37:35 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -20,20 +20,6 @@ const Footer = () => {
           <FaPinterest className="text-red-600 hover:text-red-800" size={24} />
         </a>
       </div>
-      <div className="flex space-x-4 mt-2">
-        <a href="https://www.facebook.com/Catchmytomb" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
-          <FaFacebook className="text-blue-600 hover:text-blue-800" size={24} />
-        </a>
-        <a href="https://www.instagram.com/catchmytombfrance/" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
-          <FaInstagram className="text-pink-500 hover:text-pink-700" size={24} />
-        </a>
-        <a href="https://x.com/catchmytomb" target="_blank" rel="noopener noreferrer" aria-label="Twitter">
-          <FaTwitter className="text-blue-400 hover:text-blue-600" size={24} />
-        </a>
-        <a href="https://www.pinterest.fr/CatchMyTombFrance/" target="_blank" rel="noopener noreferrer" aria-label="Pinterest">
-          <FaPinterest className="text-red-600 hover:text-red-800" size={24} />
-        </a>
-      </div>
     </footer>
   );
 };
