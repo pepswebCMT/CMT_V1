def5f4ba6abc29a0ccaa0e05a73113c76136478b
akimi987 <75440087+akimi987@users.noreply.github.com>
2024-06-10 10:47:47 +0200

    update PWA

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -2,47 +2,34 @@ const CACHE_NAME = "version-1";
 const urlsToCache = ['./index.html', './favicon.ico', './CMT-logo-02.svg'];
 
 self.addEventListener('install', (event) => {
-    console.log('Service Worker: Installing');
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then((cache) => {
-                console.log('Service Worker: Caching files');
+                console.log('Opened cache');
                 return cache.addAll(urlsToCache);
-            })
-            .catch((error) => {
-                console.error('Service Worker: Failed to cache', error);
             })
     );
 });
 
 self.addEventListener('fetch', (event) => {
-    console.log('Service Worker: Fetching', event.request.url);
     event.respondWith(
         caches.match(event.request)
             .then((response) => {
                 if (response) {
-                    console.log('Service Worker: Returning cached response', event.request.url);
                     return response;
                 }
-                console.log('Service Worker: Fetching from network', event.request.url);
-                return fetch(event.request)
-                    .catch((error) => {
-                        console.error('Service Worker: Fetch failed', error);
-                        return caches.match(''); // Optionally return a fallback page
-                    });
+                return fetch(event.request).catch(() => caches.match(''));
             })
     );
 });
 
 self.addEventListener('activate', (event) => {
-    console.log('Service Worker: Activating');
     const cacheWhitelist = [CACHE_NAME];
     event.waitUntil(
         caches.keys().then((cacheNames) => {
             return Promise.all(
                 cacheNames.map((cacheName) => {
-                    if (!cacheWhitelist.includes(cacheName)) {
-                        console.log('Service Worker: Deleting old cache', cacheName);
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
                         return caches.delete(cacheName);
                     }
                 })
