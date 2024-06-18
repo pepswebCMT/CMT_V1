699749f34cf35429ea76032b32b350b200968bc1
akimi987 <75440087+akimi987@users.noreply.github.com>
2024-06-18 10:23:22 +0200

    updated PWA

--- src/pages/CoverPage.jsx
+++ src/pages/CoverPage.jsx
@@ -73,23 +73,28 @@ const CoverPage = () => {
         </button>
         {!isAppInstalled && (
           <button
-            className="w-1/2 max-w-80 p-2 text-xl font-bold bg-mandarin-100 dark:bg-mandarin-600 text-white rounded-xl relative" // Ajouter relative pour le positionnement relatif
+            className="w-1/2 max-w-80 p-2 text-xl font-bold bg-mandarin-100 dark:bg-mandarin-600 text-white rounded-xl relative"
             onClick={handleInstall}
           >
             {t("cover_app")}
-            <FaQuestionCircle size={24} onClick={handleHelp} style={{ position: 'absolute', top: 4, right: 4, cursor: 'pointer' }} />
+            <FaQuestionCircle size={24} onClick={handleHelp} className="absolute top-4 right-4 cursor-pointer" />
           </button>
         )}
       </div>
 
       {showHelpDialog && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-          <div className="bg-white p-4 rounded-lg text-center">
-            <h2 className="text-lg font-bold mb-2">Installation de l'application</h2>
-            <p className="text-lg mb-4">
-              Si vous avez des problèmes avec le bouton, vous pouvez ouvrir le menu de votre navigateur en cliquant sur les 3 points situés en haut à droite de votre navigateur et sélectionner "Ajouter à l'écran d'accueil" ou "Installer l'application".
-            </p>
-            <button onClick={handleCloseHelpDialog} className="bg-blue-500 text-white px-4 py-2 rounded-lg mx-auto block">Fermer</button>
+          <div className="bg-white p-6 rounded-lg text-center max-w-lg mx-4">
+            <h2 className="text-xl font-bold mb-4">Installation de l'application</h2>
+            <div className="text-justify text-lg mb-4 space-y-4">
+              <p className="mb-4">
+                <span className="font-bold">Pour Android:</span> Ouvrez le menu de votre navigateur en cliquant sur les trois points en haut à droite, puis sélectionnez <span className="font-bold">"Ajouter à l'écran d'accueil"</span> ou <span className="font-bold">"Installer l'application"</span>.
+              </p>
+              <p>
+                <span className="font-bold">Pour iPhone:</span> Ouvrez Safari, appuyez sur l'icône de partage en bas de l'écran (le carré avec une flèche vers le haut), puis sélectionnez <span className="font-bold">"Ajouter à l'écran d'accueil"</span>.
+              </p>
+            </div>
+            <button onClick={handleCloseHelpDialog} className="bg-blue-500 text-white px-4 py-2 rounded-lg">Fermer</button>
           </div>
         </div>
       )}
