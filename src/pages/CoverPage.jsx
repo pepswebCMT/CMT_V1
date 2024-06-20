e1260e4255d34d60702bd584d5e36d349e87d415
akimi987 <75440087+akimi987@users.noreply.github.com>
2024-06-20 11:31:36 +0200

    restore cover

--- src/pages/CoverPage.jsx
+++ src/pages/CoverPage.jsx
@@ -1,65 +1,104 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-
+import React, { useState, useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import { useTranslation } from "react-i18next";
+import { FaQuestionCircle } from 'react-icons/fa';
 
 const CoverPage = () => {
-  const [deferredPrompt, setDeferredPrompt] = useState(null);
   const [isAppInstalled, setIsAppInstalled] = useState(false);
+  const [showHelpDialog, setShowHelpDialog] = useState(false);
   const navigate = useNavigate();
+  const deferredPrompt = useRef(null);
+  const { t } = useTranslation();
 
   useEffect(() => {
-    const handleAppInstalled = () => {
-      setIsAppInstalled(true);
-    };
-
     const handleBeforeInstallPrompt = (event) => {
       event.preventDefault();
-      setDeferredPrompt(event);
+      deferredPrompt.current = event;
+      setIsAppInstalled(false);
+      console.log("beforeinstallprompt event captured");
     };
 
-    const mediaQueryList = window.matchMedia('(display-mode: standalone)');
-    setIsAppInstalled(mediaQueryList.matches);
-
-    window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
-    window.addEventListener('appinstalled', handleAppInstalled);
+    const mediaQueryList = window.matchMedia("(display-mode: standalone)");
+    if (mediaQueryList.matches) {
+      setIsAppInstalled(true);
+    } else {
+      window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
+    }
 
     return () => {
-      window.removeEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
-      window.removeEventListener('appinstalled', handleAppInstalled);
+      window.removeEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
     };
   }, []);
 
   const handleInstall = () => {
-    if (deferredPrompt) {
-      deferredPrompt.prompt();
-      deferredPrompt.userChoice.then((choiceResult) => {
-        if (choiceResult.outcome === 'accepted') {
-          console.log('L\'application a été installée');
+    if (deferredPrompt.current) {
+      deferredPrompt.current.prompt();
+      deferredPrompt.current.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === "accepted") {
+          console.log("L'application a été installée");
           setIsAppInstalled(true);
         } else {
-          console.log('L\'utilisateur a annulé l\'installation');
+          console.log("L'utilisateur a annulé l'installation");
         }
-        setDeferredPrompt(null);
+        deferredPrompt.current = null;
       });
     }
   };
 
   const navigateToStart = () => {
-    navigate('/');
+    navigate("/home");
+  };
+
+  const handleHelp = () => {
+    setShowHelpDialog(true);
+  };
+
+  const handleCloseHelpDialog = () => {
+    setShowHelpDialog(false);
   };
 
   return (
-    <div className="w-full h-96 p-5 flex flex-col justify-around items-center">
-      <h1 className="w-full font-josefinBold font-bold text-2xl text-center">Welcome to CatchMyTomb, the app that lets you find the resting place of your favourite celebrities!!!</h1>
-      <button className="w-1/2 max-w-80 p-2 text-xl font-bold bg-blue-500 text-white rounded-xl" onClick={navigateToStart}>
-        Get start
-      </button>
-      {!isAppInstalled && deferredPrompt && (
-        <button className="w-1/2 max-w-80 p-2 text-xl font-bold bg-mandarin-100 dark:bg-mandarin-600 text-white rounded-xl relative" onClick={handleInstall}>
-          Install App
+    <section className="w-full pt-28 dark:bg-dark-200 font-josefin">
+      <Navbar />
+      <div className="w-full h-96 p-5 flex flex-col justify-around items-center">
+        <h1 className="w-full font-josefinBold font-bold text-2xl text-center">
+          {t("cover_welcome")}
+        </h1>
+        <button
+          className="w-1/2 max-w-80 p-2 text-xl font-bold bg-blue-500 text-white rounded-xl"
+          onClick={navigateToStart}
+        >
+          {t("cover_browse")}
         </button>
+        {!isAppInstalled && (
+          <button
+            className="w-1/2 max-w-80 p-2 text-xl font-bold bg-mandarin-100 dark:bg-mandarin-600 text-white rounded-xl relative"
+            onClick={handleInstall}
+          >
+            {t("cover_app")}
+            <FaQuestionCircle size={24} onClick={handleHelp} style={{ position: 'absolute', top: 4, right: 4, cursor: 'pointer' }} />
+          </button>
+        )}
+      </div>
+
+      {showHelpDialog && (
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+          <div className="bg-white p-6 rounded-lg text-center max-w-lg mx-4">
+            <h2 className="text-xl font-bold mb-4">Installation de l'application</h2>
+            <div className="text-justify text-lg mb-4 space-y-4">
+              <p className="mb-4">
+                <span className="font-bold">Pour Android:</span> Ouvrez le menu de votre navigateur en cliquant sur les trois points en haut à droite, puis sélectionnez <span className="font-bold">"Ajouter à l'écran d'accueil"</span> ou <span className="font-bold">"Installer l'application"</span>.
+              </p>
+              <p>
+                <span className="font-bold">Pour iPhone:</span> Sous Safari, appuyez sur l'icône de partage en bas de l'écran (le carré avec une flèche vers le haut), puis sélectionnez <span className="font-bold">"Ajouter à l'écran d'accueil"</span>.
+              </p>
+            </div>
+            <button onClick={handleCloseHelpDialog} className="bg-blue-500 text-white px-4 py-2 rounded-lg">Fermer</button>
+          </div>
+        </div>
       )}
-    </div>
+    </section>
   );
 };
 
